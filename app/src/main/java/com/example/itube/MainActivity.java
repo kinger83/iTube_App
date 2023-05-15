99eb69b52208aa16c396c9036a3d212c147543fe
kinger83 <kinger83@outlook.com>
2023-05-15 15:22:56 +1000

    login set up

--- app/src/main/java/com/example/itube/MainActivity.java
+++ app/src/main/java/com/example/itube/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.itube;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,9 +11,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.itube.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +26,37 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        binding.mainProgressBar.setVisibility(View.GONE);
+        mAuth = FirebaseAuth.getInstance();
 
         binding.mainLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                binding.mainProgressBar.setVisibility(View.VISIBLE);
                 String email = binding.mainEmailText.getText().toString();
                 String password = binding.mainPasswordText.getText().toString();
-                if(!validateInput(email, password)) return;
+                if(!validateInput(email, password)){
+                    binding.mainProgressBar.setVisibility(View.GONE);
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    binding.mainProgressBar.setVisibility(View.GONE);
+                                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                                else{
+                                    binding.mainProgressBar.setVisibility(View.GONE);
+                                    Toast.makeText(MainActivity.this, "Sign in Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
-                
+
 
 
             }
